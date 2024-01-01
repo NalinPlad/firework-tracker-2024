6e1f8c3a36eec08bfee0fca22fed76bb834f8b8d
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2023-12-31 20:04:42 -0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{io::{self, Write}, time::Duration, fs::{File,OpenOptions}};
+use text_io::read;
+use chrono::Utc;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    const FILENAME: &str = "data.csv";
+
+    let mut data_file = OpenOptions::new()
+        .append(true)
+        .open(FILENAME)
+        .expect("cannot open file");
+
+    // create a loop that waits for the user to press enter
+    loop {
+        print!("\nClick enter to record a reading");
+        let _: String = read!("{}\n");
+
+        let data = chrono::Local::now();
+        data_file
+            .write((Utc::now().timestamp_millis().to_string() + "\n").as_bytes())
+            .expect("write failed");
+
+        println!("Reading recorded at {}", data);
+    }
+
 }
